061ce6cb9222e0c8e340d36aef266d47d6a8eeca
Fedor Serkov <serkovfb@gmail.com>
2021-01-06 16:22:09 +1000

    add fibonacci praxis

---
+++ fibonacci.py
@@ -0,0 +1,23 @@
+# Kent Beck
+# Test-driven development by example
+
+import unittest
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+class TestFibonacci(unittest.TestCase):
+    def test_fibonacci(self):
+        cases = (0, 0), (1, 1), (2, 1), (3, 2)
+        for n, res in cases:
+            self.assertEqual(fib(n), res)
+
+
+if __name__ == '__main__':
+    unittest.main()
