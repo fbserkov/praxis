185e85e8ed730a71038ae760333057152ff9d378
Fedor Serkov <serkovfb@gmail.com>
2021-01-07 20:39:25 +1000

    removed classes: Dollar, Franc removed tests: test_different_class_equality, test_franc_multiplication

--- money_example.py
+++ money_example.py
@@ -30,43 +30,23 @@ class Money:
 
     @staticmethod
     def dollar(amount):
-        return Dollar(amount, 'USD')
+        return Money(amount, 'USD')
 
     @staticmethod
     def franc(amount):
-        return Franc(amount, 'CHF')
-
-
-class Dollar(Money):
-    def __init__(self, amount, currency):
-        Money.__init__(self, amount, currency)
-
-
-class Franc(Money):
-    def __init__(self, amount, currency):
-        Money.__init__(self, amount, currency)
+        return Money(amount, 'CHF')
 
 
 class MoneyTest(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Money.dollar(5) == Money.dollar(5))
         self.assertFalse(Money.dollar(5) == Money.dollar(6))
-        self.assertTrue(Money.franc(5) == Money.franc(5))
-        self.assertFalse(Money.franc(5) == Money.franc(6))
         self.assertFalse(Money.dollar(5) == Money.franc(5))
 
-    def test_different_class_equality(self):
-        self.assertTrue(Money(10, 'CHF') == Franc(10, 'CHF'))
-
-    def test_dollar_multiplication(self):
+    def test_multiplication(self):
         five = Money.dollar(5)
         self.assertEqual(Money.dollar(5 * 2), five.times(2))
         self.assertEqual(Money.dollar(5 * 3), five.times(3))
-
-    def test_franc_multiplication(self):
-        five = Money.franc(5)
-        self.assertEqual(Money.franc(5 * 2), five.times(2))
-        self.assertEqual(Money.franc(5 * 3), five.times(3))
 
     def test_currency(self):
         self.assertEqual('USD', Money.dollar(1).currency())
