37f6566f18c085166b7fc3b7460497d5ea0a47ec
Fedor Serkov <serkovfb@gmail.com>
2021-01-07 20:27:14 +1000

    Chapter 10 result

--- money_example.py
+++ money_example.py
@@ -25,6 +25,9 @@ class Money:
     def currency(self):
         return self._currency
 
+    def times(self, multiplier):
+        return Money(self._amount * multiplier, self._currency)
+
     @staticmethod
     def dollar(amount):
         return Dollar(amount, 'USD')
@@ -38,16 +41,10 @@ class Dollar(Money):
     def __init__(self, amount, currency):
         Money.__init__(self, amount, currency)
 
-    def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
-
 
 class Franc(Money):
     def __init__(self, amount, currency):
         Money.__init__(self, amount, currency)
-
-    def times(self, multiplier):
-        return Money(self._amount * multiplier, self._currency)
 
 
 class MoneyTest(unittest.TestCase):
