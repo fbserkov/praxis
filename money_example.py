d643e9f5b352d3319545ca59975e5505f75bdc88
Fedor Serkov <serkovfb@gmail.com>
2021-01-08 08:28:22 +1000

    separate tests from code

--- money_example.py
+++ money_example.py
@@ -1,10 +1,3 @@
-# Kent Beck
-# Test-driven development by example
-# Section I: Money example
-
-import unittest
-
-
 class Expression:
     pass
 
@@ -46,30 +39,3 @@ class Bank:
     @staticmethod
     def reduce(source, to):
         return Money.dollar(5 + 5)
-
-
-class MoneyExampleTest(unittest.TestCase):
-    def test_currency(self):
-        self.assertEqual('USD', Money.dollar(1).currency())
-        self.assertEqual('CHF', Money.franc(1).currency())
-
-    def test_equality(self):
-        self.assertTrue(Money.dollar(5) == Money.dollar(5))
-        self.assertFalse(Money.dollar(5) == Money.dollar(6))
-        self.assertFalse(Money.dollar(5) == Money.franc(5))
-
-    def test_multiplication(self):
-        five = Money.dollar(5)
-        self.assertEqual(Money.dollar(5 * 2), five.times(2))
-        self.assertEqual(Money.dollar(5 * 3), five.times(3))
-
-    def test_simple_addition(self):
-        five = Money.dollar(5)
-        result = five.plus(five)
-        bank = Bank()
-        reduced = bank.reduce(result, 'USD')
-        self.assertEqual(Money.dollar(5 + 5), reduced)
-
-
-if __name__ == '__main__':
-    unittest.main()
