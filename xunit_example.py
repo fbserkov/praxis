e044560b86a4812862a8b74805865e0c7511ff37
Fedor Serkov <serkovfb@gmail.com>
2021-01-08 19:19:13 +1000

    test_setup (passed)

--- xunit_example.py
+++ xunit_example.py
@@ -6,15 +6,23 @@ class TestCase:
     def __init__(self, name):
         self._name = name
 
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         method = getattr(self, self._name)
         method()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
+        self.was_setup = None
         self.was_run = None
         TestCase.__init__(self, name)
+
+    def setup(self):
+        self.was_setup = 1
 
     def test_method(self):
         self.was_run = 1
