64923f86cbb1a9770e81670ddade69e5d393c608
Fedor Serkov <serkovfb@gmail.com>
2021-01-08 18:36:03 +1000

    dynamic call

--- xunit_example.py
+++ xunit_example.py
@@ -5,15 +5,17 @@
 class WasRun:
     def __init__(self, name):
         self.was_run = None
+        self._name = name
 
     def run(self):
-        self.test_methode()
+        method = getattr(self, self._name)
+        method()
 
-    def test_methode(self):
+    def test_method(self):
         self.was_run = 1
 
 
-test = WasRun('test_methode')
+test = WasRun('test_method')
 print(test.was_run)
 test.run()
 print(test.was_run)
