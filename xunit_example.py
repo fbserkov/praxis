117b31605ec2dfe030d6836ee7925c5fc2082ba4
Fedor Serkov <serkovfb@gmail.com>
2021-01-08 18:27:26 +1000

    first success

--- xunit_example.py
+++ xunit_example.py
@@ -2,6 +2,14 @@
 # Test-driven development by example
 # Section II: xUnit example
 
+class WasRun:
+    def __init__(self, name):
+        self.was_run = None
+
+    def test_methode(self):
+        self.was_run = 1
+
+
 test = WasRun('test_methode')
 print(test.was_run)
 test.test_methode()
