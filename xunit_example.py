5ce5e81d544806d892cdb0a0dc2d39f16c6850f4
Fedor Serkov <serkovfb@gmail.com>
2021-01-08 19:12:13 +1000

    add test_setup (failed)

--- xunit_example.py
+++ xunit_example.py
@@ -27,5 +27,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert test.was_run
 
+    def test_setup(self):
+        test = WasRun('test_method')
+        test.run()
+        assert test.was_setup
+
 
 TestCaseTest('test_running').run()
+TestCaseTest('test_setup').run()
