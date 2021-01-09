f64da0e760bda7a8babaa2a763b1d924e6152e94
Fedor Serkov <serkovfb@gmail.com>
2021-01-09 10:19:34 +1000

    add test_failed_result_formatting

--- xunit_example.py
+++ xunit_example.py
@@ -4,13 +4,17 @@
 
 class TestResult:
     def __init__(self):
-        self.run_count = 0
+        self._run_count = 0
+        self._error_count = 0
 
     def test_started(self):
-        self.run_count += 1
+        self._run_count += 1
+
+    def test_failed(self):
+        self._error_count += 1
 
     def summary(self):
-        return f'{self.run_count} run, 0 failed'
+        return f'{self._run_count} run, {self._error_count} failed'
 
 
 class TestCase:
@@ -66,7 +70,14 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert '1 run, 1 failed' == result.summary()
 
+    def test_failed_result_formatting(self):
+        result = TestResult()
+        result.test_started()
+        result.test_failed()
+        assert '1 run, 1 failed' == result.summary()
+
 
 TestCaseTest('test_template_method').run()
 TestCaseTest('test_result').run()
-TestCaseTest('test_failed_result').run()
+# TestCaseTest('test_failed_result').run()
+TestCaseTest('test_failed_result_formatting').run()
