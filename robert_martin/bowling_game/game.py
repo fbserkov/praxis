7f68c090acd72b8897f2a0984dddfbcfe81930ae
Fedor Serkov <serkovfb@gmail.com>
2021-01-10 11:50:08 +1000

    Game: test_four_throws_no_mark (passed)

--- robert_martin/bowling_game/game.py
+++ robert_martin/bowling_game/game.py
@@ -1,12 +1,21 @@
 class Game:
     def __init__(self):
+        self._current_throw = 0
+        self._throws = 21 * [0]
         self._score = 0
 
     def add(self, pins):
+        self._throws[self._current_throw] = pins
+        self._current_throw += 1
         self._score += pins
 
     def score_for_frame(self, frame):
-        return 0
+        score, ball = 0, 0
+        while frame > 0 and ball < self._current_throw:
+            score += self._throws[ball] + self._throws[ball + 1]
+            frame -= 1
+            ball += 2
+        return score
 
     def get_score(self):
         return self._score
