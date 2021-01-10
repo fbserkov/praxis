ec28bfc02fca1a3a241de1a67b7fddd17cd6b7be
Fedor Serkov <serkovfb@gmail.com>
2021-01-10 10:55:57 +1000

    add __init__.py for comment

---
+++ robert_martin/bowling_game/__init__.py
@@ -0,0 +1,2 @@
+# Agile Software Development: Principles, Patterns and Practices
+# Chapter 6: A programming Episode
--- robert_martin/bowling_game/test_frame.py
+++ robert_martin/bowling_game/test_frame.py
@@ -1,6 +1,3 @@
-# Agile Software Development: Principles, Patterns and Practices
-# Chapter 6: A programming Episode
-
 import unittest
 
 
