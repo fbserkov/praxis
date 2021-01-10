4369b1178ea6acf8eef2f79c0a94fe729fbb594e
Fedor Serkov <serkovfb@gmail.com>
2021-01-10 10:53:22 +1000

    rename to frame

---
+++ robert_martin/test_frame.py
@@ -0,0 +1,15 @@
+# Agile Software Development: Principles, Patterns and Practices
+# Chapter 6: A programming Episode
+
+import unittest
+
+
+class TestFrame(unittest.TestCase):
+    # throw_ball(6)
+    # throw_ball(3)
+    # assertEqual(6 + 3, get_score())
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
