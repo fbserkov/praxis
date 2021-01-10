8ec424becd3463d385bd27ff82b1bacafc73ec73
Fedor Serkov <serkovfb@gmail.com>
2021-01-10 12:10:22 +1000

    use TestGame.setUp (fixture)

--- robert_martin/bowling_game/test_game.py
+++ robert_martin/bowling_game/test_game.py
@@ -3,25 +3,25 @@ from game import Game
 
 
 class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.g = Game()
+
     def test_one_throw(self):
-        g = Game()
-        g.add(5)
-        self.assertEqual(5, g.get_score())
+        self.g.add(5)
+        self.assertEqual(5, self.g.get_score())
 
     def test_two_throws_no_mark(self):
-        g = Game()
-        g.add(5)
-        g.add(4)
-        self.assertEqual(5 + 4, g.get_score())
+        self.g.add(5)
+        self.g.add(4)
+        self.assertEqual(5 + 4, self.g.get_score())
 
     def test_four_throws_no_mark(self):
-        g = Game()
-        g.add(5)
-        g.add(4)
-        g.add(7)
-        g.add(2)
-        self.assertEqual(5 + 4, g.score_for_frame(1))
-        self.assertEqual(5 + 4 + 7 + 2, g.score_for_frame(2))
+        self.g.add(5)
+        self.g.add(4)
+        self.g.add(7)
+        self.g.add(2)
+        self.assertEqual(5 + 4, self.g.score_for_frame(1))
+        self.assertEqual(5 + 4 + 7 + 2, self.g.score_for_frame(2))
 
 
 if __name__ == '__main__':
