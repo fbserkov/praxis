5f78ded1e574d6fb18bdd2bf9d65ba3022860c57
Fedor Serkov <serkovfb@gmail.com>
2021-01-12 11:46:17 +1000

    type checking in PayrollDatabase class

--- robert_martin/payroll_system/payroll_database.py
+++ robert_martin/payroll_system/payroll_database.py
@@ -1,12 +1,15 @@
+from employee import EmpId, Employee
+
+
 class PayrollDatabase:
     def __init__(self):
         self._employees = {}
 
-    def get_employee(self, emp_id):
+    def get_employee(self, emp_id: EmpId) -> Employee:
         return self._employees[emp_id]
 
-    def add_employee(self, emp_id, employee):
-        self._employees[emp_id] = employee
+    def add_employee(self, emp_id: EmpId, e: Employee):
+        self._employees[emp_id] = e
 
     def clear(self):
         self._employees.clear()
