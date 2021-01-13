07c3ea7e97c3440aa0edeae5668ecee485285e3d
Fedor Serkov <serkovfb@gmail.com>
2021-01-13 15:23:23 +1000

    test_change_direct_transaction

---
+++ robert_martin/payroll_system/change_method_transaction.py
@@ -0,0 +1,24 @@
+from change_employee_transaction import ChangeEmployeeTransaction
+from employee import EmpId, Employee
+from payment_method import DirectMethod, PaymentMethod
+
+
+class ChangeMethodTransaction(ChangeEmployeeTransaction):
+    def __init__(self, emp_id: EmpId):
+        ChangeEmployeeTransaction.__init__(self, emp_id)
+
+    def change(self, e: Employee):
+        e.set_method(self.get_method())
+
+    def get_method(self) -> PaymentMethod:
+        return PaymentMethod()
+
+
+class ChangeDirectTransaction(ChangeMethodTransaction):
+    def __init__(self, emp_id: EmpId, bank, account):
+        ChangeMethodTransaction.__init__(self, emp_id)
+        self._bank = bank
+        self._account = account
+
+    def get_method(self) -> DirectMethod:
+        return DirectMethod(self._bank, self._account)
--- robert_martin/payroll_system/payment_method.py
+++ robert_martin/payroll_system/payment_method.py
@@ -2,5 +2,17 @@ class PaymentMethod:
     pass
 
 
+class DirectMethod(PaymentMethod):
+    def __init__(self, bank, account):
+        self._bank = bank
+        self._account = account
+
+    def get_bank(self):
+        return self._bank
+
+    def get_account(self):
+        return self._account
+
+
 class HoldMethod(PaymentMethod):
     pass
